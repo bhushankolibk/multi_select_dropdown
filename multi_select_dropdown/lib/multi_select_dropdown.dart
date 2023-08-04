a2bb5b2b54f20a538cbe9fa304831763e9700314
Bhushan Koli <bhushankoli2014@gmail.com>
2023-08-04 16:50:16 +0530

    push

--- multi_select_dropdown/lib/multi_select_dropdown.dart
+++ multi_select_dropdown/lib/multi_select_dropdown.dart
@@ -1,7 +1,174 @@
 library multi_select_dropdown;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class MultiSelectDropdown extends StatefulWidget {
+  const MultiSelectDropdown({
+    Key? key,
+    required this.dropdownValueList,
+    required this.selectedTileColor,
+    required this.onChanged,
+    this.deleteIconColor,
+    this.hint,
+    this.hintStyle,
+    this.itemTextStyle,
+    this.decoration,
+    this.trailingIcon,
+    this.selectedItemBackgroundColor,
+    this.selectedItemTextStyle,
+  }) : super(key: key);
+
+  final List<String> dropdownValueList;
+  final Color selectedTileColor;
+  /// Called when the user selects an item and return list<String>
+  final ValueChanged<List<String>> onChanged;
+  final Color? deleteIconColor;
+  final String? hint;
+
+
+  final TextStyle? hintStyle;
+  final TextStyle? itemTextStyle;
+  final Decoration? decoration;
+  final Icon? trailingIcon;
+  final Color? selectedItemBackgroundColor;
+  final TextStyle? selectedItemTextStyle;
+
+  @override
+  State<MultiSelectDropdown> createState() => _MultiSelectDropdownState();
+}
+
+class _MultiSelectDropdownState extends State<MultiSelectDropdown> {
+  late List<String> list;
+  late List<String> listOFSelectedTeam = [];
+  late Color selectedTileColor;
+
+  @override
+  void initState() {
+    list = widget.dropdownValueList;
+    selectedTileColor = widget.selectedTileColor;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter stateSetter) {
+        return InkWell(
+          onTap: () {
+            showDialog(
+                context: context,
+                builder: (ctx) {
+                  return Dialog(child: StatefulBuilder(
+                      builder: (BuildContext ctx, StateSetter state) {
+                    return Scrollbar(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: list.map((e) {
+                            return SizedBox(
+                              width: MediaQuery.of(context).size.width,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ListTile(
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      horizontal: 16.0),
+                                  title: Text(
+                                    e,
+                                    style: widget.itemTextStyle ??
+                                        const TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400,
+                                          color: Colors.black,
+                                        ),
+                                  ),
+                                  tileColor: listOFSelectedTeam.contains(e)
+                                      ? selectedTileColor
+                                      : null,
+                                  onTap: () {
+                                    if (listOFSelectedTeam.contains(e)) {
+                                      listOFSelectedTeam.remove(e);
+                                    } else {
+                                      listOFSelectedTeam.add(e);
+                                    }
+                                    widget.onChanged(listOFSelectedTeam);
+
+                                    stateSetter(() {});
+                                    state(() {});
+                                  },
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    );
+                  }));
+                });
+          },
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: widget.decoration ??
+                const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                        width: 2.0,
+                        color:  Colors.grey
+                            ),
+                  ),
+                ),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: listOFSelectedTeam.isEmpty
+                      ? Text(
+                          widget.hint ?? "Select Item",
+                          style: widget.hintStyle ??
+                              const TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black),
+                        )
+                      : Wrap(
+                          children: listOFSelectedTeam.map(
+                          (e) {
+                            return Padding(
+                              padding: const EdgeInsets.only(
+                                left: 2,
+                                right: 2,
+                              ),
+                              child: Chip(
+                                label: Text("$e ",
+                                    style: widget.selectedItemTextStyle ?? TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black.withOpacity(0.8))),
+                                deleteIcon: const Icon(
+                                  Icons.delete,
+                                ),
+                                backgroundColor: widget.selectedItemBackgroundColor ?? Colors.grey,
+                                deleteIconColor: widget.deleteIconColor ?? Colors.black,
+                                onDeleted: () {
+                                  stateSetter(() {
+                                    listOFSelectedTeam.remove(e);
+                                    widget.onChanged(listOFSelectedTeam);
+                                  });
+                                },
+                              ),
+                            );
+                          },
+                        ).toList()),
+                ),
+               widget.trailingIcon ?? const Icon(
+                 Icons.arrow_drop_down,
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
